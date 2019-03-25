c062aa80d0ba07c54325c0baeaf83328782ed690
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-25 19:16:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,7 @@ def big_shoe_rebounds
     team_data[:players].each do |player_name, data|
       if data[:shoe] > shaq
         shaq = data[:shoe]
-        rebound = data[:rebound]
+        rebound = data[:rebounds]
       end
     end
   end
