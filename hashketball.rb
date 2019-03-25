a621261e1d0f65af3706fb31251cc6553ea4052a
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-25 19:02:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,7 +139,7 @@ def shoe_size(name)
 end
 
 def team_colors(name)
-  game_hash.collect do |location, team_data|
+  game_hash.each do |location, team_data|
     if team_data[:team_name] == name
       team_data[:colors]
     end
